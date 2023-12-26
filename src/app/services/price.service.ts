7efc64efac12d613876ea904a9d8b43c80c8dd05
raiblocksone <138157144+raiblocksone@users.noreply.github.com>
2023-12-26 23:48:49 +0100

    show price again

--- src/app/services/price.service.ts
+++ src/app/services/price.service.ts
@@ -30,7 +30,8 @@ export class PriceService {
     //const currencyPrice = quote[currency.toLowerCase()];
     //const btcPrice = quote.btc;
     const quote = response;
-    const currencyPrice = quote[4].midPriceUSD.toFixed(8); 
+    const currencyPrice = quote[4].midPriceUSD;
+    //const currencyPrice = quote[4].midPriceUSD.toFixed(4); 
 
     this.price.lastPrice = currencyPrice;
     //this.price.lastPrice = 0;
